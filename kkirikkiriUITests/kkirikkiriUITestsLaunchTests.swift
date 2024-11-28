ac3cfac6c265d6c3f6f5a33ba9a66c5474fce15b
BeomSoo Park <linoleca@naver.com>
2024-11-29 02:01:19 +0900

    Initial Commit

---
+++ kkirikkiriUITests/kkirikkiriUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  kkirikkiriUITestsLaunchTests.swift
+//  kkirikkiriUITests
+//
+//  Created by 박범수 on 11/29/24.
+//
+
+import XCTest
+
+final class kkirikkiriUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
